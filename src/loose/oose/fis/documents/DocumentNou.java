9aab1aa9947b6bf380179f48ae404ede4f659160
Cojocariu Flavius <cojocariu.flavius23@gmail.com>
2020-03-18 17:56:32 +0200

    Done some changes

---
+++ src/loose/oose/fis/documents/DocumentNou.java
@@ -0,0 +1,30 @@
+package loose.oose.fis.documents;
+
+import java.util.Arrays;
+
+public class DocumentNou extends Document {
+
+    public DocumentNou(String[] continut) {
+        super(continut);
+    }
+
+    @Override
+    public String[] analizeaza() {
+        String[] res = new String[continut.length];
+        int      pos = 0;
+
+        for (String cuvant : continut) {
+            if (!cuvant.contains(":")) {
+                res[pos] = cuvant;
+                pos++;
+            }
+        }
+
+        return Arrays.copyOf(res, pos);
+    }
+
+    @Override
+    public String toString() {
+        return "DocumentNou " + super.toString();
+    }
+}
